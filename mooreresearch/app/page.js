258e2ef46afff948b533a9295e1f034a46c55888
Matthew Moore <matthew.moore@berenberg.com>
2026-07-03 11:20:56 +0100

    Update: redeploying all

--- mooreresearch/app/page.js
+++ mooreresearch/app/page.js
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
-import { RoomProvider, ClientSideSuspense, LiveblocksProvider, useThreads } from "@liveblocks/react/suspense";
-import { FloatingComposer, FloatingThreads, CommentsPopover } from "@liveblocks/react-ui";
-import { liveblocksConfig, LiveblocksPlugin } from "@liveblocks/react-lexical";
+import { ClientSideSuspense } from "@liveblocks/react/suspense";
+import { LiveblocksProvider, RoomProvider, useThreads } from "@liveblocks/react";
+import { liveblocksConfig, LiveblocksPlugin, FloatingComposer, FloatingThreads } from "@liveblocks/react-lexical";
 
 // A loading spinner
 function Loading() {
@@ -27,21 +27,20 @@ function CollaborativeEditor() {
 
   return (
     <div className="max-w-4xl mx-auto mt-10 border border-gray-300 rounded-lg shadow-lg relative">
-        <LexicalComposer initialConfig={initialConfig}>
-            <LiveblocksPlugin>
-                <div className="relative">
-                    <RichTextPlugin
-                        contentEditable={<ContentEditable className="p-5 focus:outline-none min-h-[300px]" />}
-                        placeholder={<div className="p-5 absolute top-0 left-0 pointer-events-none text-gray-400">Start typing...</div>}
-                        ErrorBoundary={LexicalErrorBoundary}
-                    />
-                    <HistoryPlugin />
-                    <CommentsPopover />
-                    <FloatingComposer />
-                    <FloatingThreads threads={threads} />
-                </div>
-            </LiveblocksPlugin>
-        </LexicalComposer>
+      <LexicalComposer initialConfig={initialConfig}>
+        <LiveblocksPlugin>
+          <div className="relative">
+            <RichTextPlugin
+              contentEditable={<ContentEditable className="p-5 focus:outline-none min-h-[300px]" />}
+              placeholder={<div className="p-5 absolute top-0 left-0 pointer-events-none text-gray-400">Start typing...</div>}
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+            <HistoryPlugin />
+            <FloatingComposer />
+            <FloatingThreads threads={threads} />
+          </div>
+        </LiveblocksPlugin>
+      </LexicalComposer>
     </div>
   );
 }
